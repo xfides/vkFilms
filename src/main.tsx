fa7667eb092150efa90cd33c5c19446eb1e3ca1c
baradmitrij <baradmitrij@yandex.ru>
2024-05-11 18:47:15 +0300

    feat: add normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "@/components/App"
-import '@/assets/global.css'
+import '@/assets/css/normalize.css'
+import '@/assets/css/global.css'
 
 console.clear();
 
