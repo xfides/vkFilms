d4c60cbdb74bad329c71e6020f0a2735a3caedf8
baradmitrij <baradmitrij@yandex.ru>
2024-05-11 18:35:17 +0300

    feat: add import aliases to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+// @ts-expect-error | TS Linter doesn't see node module 'path', but it's OK
+import path from 'path';
+
+const root = path.resolve("src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // remember to duplicate alias paths in tsconfig/compilerOptions/paths
+      // FOR EXAMPLE <"@": root> -> <"@/*" : ["./src/*"]>. This helps your IDE
+      "@": root,
+    },
+  },
 })
